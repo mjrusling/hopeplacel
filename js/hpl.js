ec4122854ed6cd65eb47b47a916d64d4e098dd65
mjrusling <33674410+mjrusling@users.noreply.github.com>
2019-08-15 20:29:57 -0400

    js testing 2

--- js/hpl.js
+++ js/hpl.js
@@ -3,7 +3,7 @@
 var savedText = "";
 function showETA(e) {
 	savedText = e.innerHTML;
-	e.innerHTML = "Next page update: 8AM EAT"
+	e.innerHTML = "Next page update: 8AM East Africa Time"
 }
 function hideETA(e) {
 	e.innerHTML = savedText;
