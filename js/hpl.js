52f66f3d4f0c1850f224503b52ce83592751b8a6
mjrusling <33674410+mjrusling@users.noreply.github.com>
2019-08-15 20:22:55 -0400

    js testing

--- js/hpl.js
+++ js/hpl.js
@@ -1,1 +1,10 @@
 // Reserved for custom JS
+
+var savedText = "";
+function showETA(e) {
+	savedText = e.innerHTML;
+	e.innerHTML = "Next page update: 8AM EAT"
+}
+function hideETA(e) {
+	e.innerHTML = savedText;
+}
